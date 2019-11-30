2a8dde68a6aa0346a440991792d7121d8e743077
Ruben Soerdien <ruben.soerdien@hotmail.com>
2019-11-30 23:05:31 +0100

    socket.io url from different place

--- src/renderer/plugins/socket.io.js
+++ src/renderer/plugins/socket.io.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
-
+import { remote } from 'electron'
 export default ({ store }) => {
   console.log(12345)
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: 'http://localhost:4001/',
+    connection: remote.getGlobal('sharedObj').socketUrl,
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
